74710702b7e47c013eea4513d35172d74cac2c7e
DudaAraujo14 <rm560944@fiap.com.br>
2025-10-19 15:55:46 -0300

    chore: configura projeto base do Spring Boot e dependências iniciais

---
+++ src/main/java/com/example/cp2/Cp2Application.java
@@ -0,0 +1,11 @@
+package com.example.cp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Cp2Application.class, args);
+    }
+}
